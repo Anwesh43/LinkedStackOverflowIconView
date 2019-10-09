c78214592758718a5c51ab6be7d1af50465b839f
Anwesh43 <anweshthecool0@gmail.com>
2019-10-09 10:32:15 +0530

    adding constants and importing libraries

--- stackoverflowiconview/src/main/java/com/anwesh/uiprojects/stackoverflowiconview/StackOverflowIconView.kt
+++ stackoverflowiconview/src/main/java/com/anwesh/uiprojects/stackoverflowiconview/StackOverflowIconView.kt
@@ -4,3 +4,21 @@ package com.anwesh.uiprojects.stackoverflowiconview
  * Created by anweshmishra on 09/10/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val lines : Int = 5
+val deg : Float = 90f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val lineColor : Int = Color.parseColor("#FF5722")
+val boxColor : Int = Color.parseColor("#212121")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
