98028282a08da4ce39d242f909022ad3d92a93ed
Anwesh43 <anweshthecool0@gmail.com>
2019-10-09 11:02:49 +0530

    adding SOINode class

--- stackoverflowiconview/src/main/java/com/anwesh/uiprojects/stackoverflowiconview/StackOverflowIconView.kt
+++ stackoverflowiconview/src/main/java/com/anwesh/uiprojects/stackoverflowiconview/StackOverflowIconView.kt
@@ -143,4 +143,47 @@ class StackOverflowIconView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SOINode(var i : Int, val state : State = State()) {
+
+        private var next : SOINode? = null
+        private var prev : SOINode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SOINode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas?.drawSOINode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SOINode {
+            var curr : SOINode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
 }
